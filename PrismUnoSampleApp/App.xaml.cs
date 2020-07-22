c2734ed1d1a02a05007fd1c213971b6b36d2d516
Kazuki Ota <kaota@microsoft.com>
2020-07-22 18:01:50 +0900

    Add project files.

---
+++ PrismUnoSampleApp/App.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Prism.Ioc;
+using Prism.Modularity;
+using Prism.Unity;
+using PrismUnoSampleApp.EnglishRestaurant;
+using PrismUnoSampleApp.Views;
+using Windows.UI.Xaml;
+
+namespace PrismUnoSampleApp
+{
+    sealed partial class App : PrismApplication
+    {
+        public App()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override UIElement CreateShell() => Container.Resolve<Shell>();
+
+        protected override void RegisterTypes(IContainerRegistry containerRegistry)
+        {
+        }
+
+        protected override void ConfigureModuleCatalog(IModuleCatalog moduleCatalog)
+        {
+            moduleCatalog.AddModule<EnglishRestaurantModule>();
+        }
+    }
+}
